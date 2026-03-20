3ae14596f654061e949770c299d5b0044b6ac8fa
joaoamorimgamer <cachorro11_nando@proton.me>
2026-03-20 13:26:14 -0300

    Changed real.c

--- real.c
+++ real.c
@@ -1,6 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	return 0;
+	if(argc > 1)
+	{
+		if(strcmp(argv[1], "--help") == 0)
+		{
+			printf("real - do nothing, succesfully\n");
+			printf("better version of 'true', by GNU Coreutils\n\n");
+			printf("Usage:\n");
+			printf("--help	display this help and exit\n");
+		}
+	}
+	return EXIT_SUCCESS;
 }
